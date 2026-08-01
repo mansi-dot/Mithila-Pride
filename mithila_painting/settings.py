e06eb0423542de2fdad676c5998ab1b34def884e
Mansi <mansi11411@gmail.com>
2026-08-01 11:55:43 +0530

    Configure Django for Render

--- mithila_painting/settings.py
+++ mithila_painting/settings.py
@@ -1,12 +1,15 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-r$l*s+*t85&532phxr2dh6sao*yf-tc%n9!ud*y3*&_i4t%@#2"
+)
 
-SECRET_KEY = 'django-insecure-r$l*s+*t85&532phxr2dh6sao*yf-tc%n9!ud*y3*&_i4t%@#2'
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
     'store',
@@ -21,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -89,6 +93,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 
